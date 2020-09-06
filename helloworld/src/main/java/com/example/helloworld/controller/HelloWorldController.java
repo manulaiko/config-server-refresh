422f4adf440bd01d4cfc8d89b59be865940376cb
manulaiko <manulaiko@gmail.com>
2020-09-06 17:42:08 +0200

    Initial commit

---
+++ helloworld/src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @Value("${app.format}")
+    private String message;
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return String.format(message, name);
+    }
+}
